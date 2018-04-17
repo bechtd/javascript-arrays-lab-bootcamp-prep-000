33bf91da91ff0eb88f579254389b0fcda0f2c0af
David Becht <bechtd@gmail.com>
2018-04-17 17:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   kittens.shift(1)
 }
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(1, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
